8991c18cc7afc5d9f426486cdfd92d13aed88660
kiki0323 <118674857+kiki0323@users.noreply.github.com>
2022-12-05 12:09:47 +0800

    ninu1-1 聲音
    移動受傷聲音

--- Assets/player.cs
+++ Assets/player.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public GameObject gameManager;//置放GameManager物件公開變數
+    public AudioClip walk;
+    public AudioClip hurt;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -18,11 +21,13 @@ public class player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.Translate(-3, 0, 0);//往左移動「3」
+            audioSource.PlayOneShot(walk);
         }
         //點擊右方向紐時
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.Translate(3, 0, 0);//往右移動「3」
+            audioSource.PlayOneShot(walk);
         }
 
     }
@@ -30,14 +35,17 @@ public class player : MonoBehaviour
     public void LbuttonDown()
     {
         transform.Translate(-3, 0, 0);
+        audioSource.PlayOneShot(walk);
     }
     public void RbuttonDown()
     {
         transform.Translate(3, 0, 0);
+        audioSource.PlayOneShot(walk);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        audioSource.PlayOneShot(hurt);
         gameManager.GetComponent<GameManager>().DecreaseHp();//扣血
         print("0");
     }
